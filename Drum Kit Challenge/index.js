b3e3ac1ba92b88ac3390c4dd2617a198843fde69
Aadarsh Naik <55293587+aadarshnaik@users.noreply.github.com>
2020-12-15 15:55:35 +0530

    Using Key Value Pair instead of switch case

--- Drum Kit Challenge/index.js
+++ Drum Kit Challenge/index.js
@@ -14,48 +14,60 @@ document.addEventListener("keydown", function(event){
     buttonAnimation(event.key);
 })
 
-function makeSound(key){
-switch (key) {
-    case "w":
-            var tom1 = new Audio('sounds/tom-1.mp3');
-            tom1.play();
-        break;
+function makeSound(key, sounds = {
+    w: 'sounds/tom-1.mp3',
+    a: 'sounds/tom-2.mp3',
+    s: 'sounds/tom-3.mp3',
+    d: 'sounds/tom-4.mp3',
+    j: 'sounds/snare.mp3',
+    k: 'sounds/crash.mp3',
+    l: 'sounds/kick-bass.mp3'
+}){
+    if (sounds[key]) (new Audio(sounds[key])).play();
+}
 
-    case "a":
-            var tom2 = new Audio('sounds/tom-2.mp3');
-            tom2.play();
-        break;
+// function makeSound(key){
+// switch (key) {
+//     case "w":
+//             var tom1 = new Audio('sounds/tom-1.mp3');
+//             tom1.play();
+//         break;
 
-    case "s":
-            var tom3 = new Audio('sounds/tom-3.mp3');
-            tom3.play();
-        break;
+//     case "a":
+//             var tom2 = new Audio('sounds/tom-2.mp3');
+//             tom2.play();
+//         break;
 
-    case "d":
-            var tom4 = new Audio('sounds/tom-4.mp3');
-            tom4.play();
-        break;
+//     case "s":
+//             var tom3 = new Audio('sounds/tom-3.mp3');
+//             tom3.play();
+//         break;
 
-    case "j":
-            var snare = new Audio('sounds/snare.mp3');
-            snare.play();
-        break;
+//     case "d":
+//             var tom4 = new Audio('sounds/tom-4.mp3');
+//             tom4.play();
+//         break;
 
-    case "k":
-            var crash = new Audio('sounds/crash.mp3');
-            crash.play();
-        break;
+//     case "j":
+//             var snare = new Audio('sounds/snare.mp3');
+//             snare.play();
+//         break;
 
-    case "l":
-            var kick = new Audio('sounds/kick-bass.mp3');
-            kick.play();
-        break;
+//     case "k":
+//             var crash = new Audio('sounds/crash.mp3');
+//             crash.play();
+//         break;
+
+//     case "l":
+//             var kick = new Audio('sounds/kick-bass.mp3');
+//             kick.play();
+//         break;
     
     
-    default:
-        break;
-}
-}
+//     default:
+//         break;
+// }
+// }
 
 
 function buttonAnimation(currentKey){
